7e976e58ad046daa9fd2d87e247f8874825e53f6
Patrick0815300 <patrick-nigrin@web.degit add .git remote add origin https://github.com/Patrick0815300/Testprojekt.git>
2024-04-25 11:50:29 +0200

    render all single Pokemon

--- script.js
+++ script.js
@@ -1,36 +1,63 @@
-let pokedex;
+let pokedex = [];
+let firstLoad = 11; 
 
 async function init() {
-    await loadPokedex();
-    renderCard();
+     await getPokedex();
+     renderCardsContainer();
 }
 
-async function loadPokedex() {
-    let url = `https://pokeapi.co/api/v2/pokemon/bulbasaur`;
+async function getPokedex() {
+    for (let i = 1; i < firstLoad; i++) {
+        await loadPokedex(i); 
+    }
+}
+
+async function loadPokedex(i) {
+    let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
     let response = await fetch(url);
-    pokedex = await response.json();
+    let responseToJson = await response.json();
+    pokedex.push(responseToJson);
+    console.log('Pokedex: ', pokedex)
 }
 
-function renderCard() {
-    let content = document.getElementById('pokemonCard')
-    content.innerHTML += renderCardTemp();
-    renderPokemonName();
-    renderPokemonType();
+
+function renderCardsContainer() {
+    let container = document.getElementById('container')
+    container.innerHTML = '';
+    for (let i = 0; i < pokedex.length; i++) {
+        const pokemon = pokedex[i];
+        container.innerHTML += renderEmptyCard(i);
+        renderCard(i);
+    }
 }
 
-function renderPokemonName() {
-    let name = pokedex['name'];
-    let nameContent = document.getElementById(`pokemonName`);
+function renderCard(i) {
+    let content = document.getElementById(`pokemonCard${i}`)
+    content.innerHTML += renderCardTemp(i);
+    renderPokemonName(i);
+    renderPokemonType(i);
+    renderPokemonID(i);
+}
+
+function renderPokemonName(i) {
+    let name = pokedex[i]['name'];
+    let nameContent = document.getElementById(`pokemonName${i}`);
     console.log('Name: ', name)
     nameContent.innerHTML = name;
 }   
 
-function renderPokemonType() {
-    let types = pokedex['types'];
-    let typeContent = document.getElementById(`pokemonTypes`);
+function renderPokemonType(i) {
+    let types = pokedex[i]['types'];
+    let typeContent = document.getElementById(`pokemonTypes${i}`);
     for (let i = 0; i < types.length; i++) {
-        const type = pokedex['types'][i]['type']['name'];
+        const type = types[i]['type']['name'];
         console.log('Type: ', type);  
         typeContent.innerHTML += `<div class="pokemonType">${type}</div>`; 
     }
+}
+
+function renderPokemonID(i) {
+    let id = pokedex[i]['id'];
+    let content = document.getElementById(`pokemonID${i}`);
+    content.innerHTML = '# ' + id;
 }
--- templates.js
+++ templates.js
@@ -1,13 +1,23 @@
-function renderCardTemp() {
+function renderCardTemp(i) {
     return /* html */ `
+    <div class="pokemon-card" id="pokemonCard${i}">
         <div class="name-and-id">
-            <span class="pokemon-name" id="pokemonName"></span>
-            <span>#001</span>
+            <span class="pokemon-name" id="pokemonName${i}"></span>
+            <span id="pokemonID${i}"></span>
         </div>
         
         <div class="type-and-img">
-            <div id="pokemonTypes"></div>
-            <div><img class="pokemon-img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png" alt="Pokemon-Img"></div>
-        </div>
+            <div id="pokemonTypes${i}"></div>
+            <div><img class="pokemon-img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${i+1}.png" alt="Pokemon-Img"></div>
+        </div>    
+    </div>  
+    `;
+}
+
+function renderEmptyCard(i) {
+    return /* html */ `
+    <div class="pokemon-card" id="pokemonCard${i}">
+
+    </div>  
     `;
 }
